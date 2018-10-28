edd7a2aba01d2f6aaa441825dea3d0f5bea0141b
CHE Yulin <yche@cse.ust.hk>
2018-10-28 12:13:57 +0800

    try java codes

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -1,32 +1,42 @@
 package com.alibabacloud.polar_race.engine.common;
 
-import com.alibabacloud.polar_race.engine.common.AbstractEngine;
 import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
-import com.alibabacloud.polar_race.engine.common.exceptions.RetCodeEnum;
 
 public class EngineRace extends AbstractEngine {
+    static ThreadLocal<Integer> integerThreadLocal = new ThreadLocal<Integer>();
+    static ThreadLocal<Integer> integerThreadLocalRead = new ThreadLocal<Integer>();
 
-	@Override
-	public void open(String path) throws EngineException {
-	}
-	
-	@Override
-	public void write(byte[] key, byte[] value) throws EngineException {
-	}
-	
-	@Override
-	public byte[] read(byte[] key) throws EngineException {
-		byte[] value = null;
-		
-		return value;
-	}
-	
-	@Override
-	public void range(byte[] lower, byte[] upper, AbstractVisitor visitor) throws EngineException {
-	}
-	
-	@Override
-	public void close() {
-	}
+    @Override
+    public void open(String path) throws EngineException {
+        System.out.println(path);
+    }
+
+    @Override
+    public void write(byte[] key, byte[] value) throws EngineException {
+        if (integerThreadLocal.get() == null) {
+            integerThreadLocal.set(0);
+        }
+        int curVal = integerThreadLocal.get();
+        if (curVal % 100000 == 0) {
+            System.out.println(Thread.currentThread().getId() + ", " + curVal);
+        }
+        integerThreadLocal.set(curVal + 1);
+    }
+
+    @Override
+    public byte[] read(byte[] key) throws EngineException {
+        byte[] value = null;
+
+        return value;
+    }
+
+    @Override
+    public void range(byte[] lower, byte[] upper, AbstractVisitor visitor) throws EngineException {
+    }
+
+    @Override
+    public void close() {
+        System.out.println(Thread.currentThread().getId());
+    }
 
 }
