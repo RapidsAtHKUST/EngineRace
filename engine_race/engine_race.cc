03d300545dff29bdd6b16f6ac22a6a6e8358e9b5
ssunah <shixuansun89@gmail.com>
2018-11-05 19:31:54 +0800

    Test append

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -338,7 +338,7 @@ namespace polar_race {
 
                 for (uint32_t j = 0; j < write_block_num; ++j) {
                     size_t write_offset = (size_t)write_file_block_offset[j] * block_size;
-                    pwrite(local_file_dp, value_buffer, block_size, write_offset);
+                    write(local_file_dp, value_buffer, block_size);
                 }
             }));
         }
@@ -405,7 +405,7 @@ namespace polar_race {
         vector<uint32_t> alignment_size_config = {4096};
         vector<uint32_t> thread_num_config = {64};
         uint32_t flag_config_num = 1;
-        vector<int> write_file_flags_config = {O_RDWR | O_DIRECT};
+        vector<int> write_file_flags_config = {O_RDWR | O_APPEND | O_TRUNC | O_DIRECT};
         vector<int> read_file_flags_config = {O_RDONLY | O_DIRECT};
 
         uint32_t count = 0;
@@ -433,35 +433,35 @@ namespace polar_race {
             delete[] file_block_offset;
         }
 
-        log_info("Close file end..");
-        count = 0;
-        for (uint32_t block_size : block_size_config) {
-            uint32_t block_num = (uint32_t)(value_file_size / block_size);
-            uint32_t* file_block_offset = new uint32_t[block_num];
-            for (uint32_t i = 0; i < block_num; ++i) {
-                file_block_offset[i] = i;
-            }
-
-            for (uint32_t shuffle_count = 0; shuffle_count < 3; ++shuffle_count) {
-                auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-
-                shuffle(file_block_offset, file_block_offset + block_num, std::default_random_engine(seed));
-            }
-
-            for (uint32_t alignment_size : alignment_size_config) {
-                for (uint32_t thread_num : thread_num_config) {
-                    for (uint32_t flag_config = 0; flag_config < flag_config_num; ++flag_config) {
-                        int write_file_flags = write_file_flags_config[flag_config];
-                        int read_file_flags = read_file_flags_config[flag_config];
-
-                        log_info("%d", count++);
-
-                        TestDevice(write_file_flags, file_block_offset, block_num,
-                                   read_file_flags, file_block_offset, block_num, thread_num, block_size, alignment_size);
-                    }
-                }
-            }
-            delete[] file_block_offset;
-        }
+//        log_info("Close file end..");
+//        count = 0;
+//        for (uint32_t block_size : block_size_config) {
+//            uint32_t block_num = (uint32_t)(value_file_size / block_size);
+//            uint32_t* file_block_offset = new uint32_t[block_num];
+//            for (uint32_t i = 0; i < block_num; ++i) {
+//                file_block_offset[i] = i;
+//            }
+//
+//            for (uint32_t shuffle_count = 0; shuffle_count < 3; ++shuffle_count) {
+//                auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+//
+//                shuffle(file_block_offset, file_block_offset + block_num, std::default_random_engine(seed));
+//            }
+//
+//            for (uint32_t alignment_size : alignment_size_config) {
+//                for (uint32_t thread_num : thread_num_config) {
+//                    for (uint32_t flag_config = 0; flag_config < flag_config_num; ++flag_config) {
+//                        int write_file_flags = write_file_flags_config[flag_config];
+//                        int read_file_flags = read_file_flags_config[flag_config];
+//
+//                        log_info("%d", count++);
+//
+//                        TestDevice(write_file_flags, file_block_offset, block_num,
+//                                   read_file_flags, file_block_offset, block_num, thread_num, block_size, alignment_size);
+//                    }
+//                }
+//            }
+//            delete[] file_block_offset;
+//        }
     }
 }  // namespace polar_race
