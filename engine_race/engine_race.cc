5ded15853f6256c16c098fa6cf4cf90f6b1efde1
ssunah <shixuansun89@gmail.com>
2018-11-12 21:00:30 +0800

    aio test 256 depth

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -779,7 +779,7 @@ namespace polar_race {
         // const size_t value_file_size = (size_t) VALUE_SIZE * 100;
         vector<uint32_t> block_size_config = {4096 * 4};
         vector<uint32_t> thread_num_config = {1};
-        vector<uint32_t> queue_depth_config = {64, 128, 256};
+        vector<uint32_t> queue_depth_config = {128, 256};
         uint32_t flag_config_num = 1;
         vector<int> write_file_flags_config = {O_CREAT | O_WRONLY | O_DIRECT};
 
