54bc5cf9c3175aee7bed4bb7ad7f6c923c91166f
CHE Yulin <yche@cse.ust.hk>
2018-11-12 12:31:09 +0800

    no log

--- engine_race/engine_race.h
+++ engine_race/engine_race.h
@@ -23,7 +23,7 @@
 #define TMP_KEY_BUFFER_SIZE (2048)
 #define KEY_READ_BLOCK_COUNT (8192)
 #define TO_UINT64(buffer) (*(uint64_t*)(buffer))
-#define BARRIER_NUM (32)
+#define BARRIER_NUM (NUM_THREADS)
 
 namespace polar_race {
     using namespace std;
