36ac478166b178bb8627d607ac4deb6e81b69651
CHE Yulin <yche@cse.ust.hk>
2018-11-26 19:46:23 +0800

    collect dstat info

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -265,7 +265,7 @@ namespace polar_race {
 // 1. Open engine
     RetCode EngineRace::Open(const std::string &name, Engine **eptr) {
 //        log_info("Consumption: %d KB, Free Mem (KB): \n%s", getValue(), exec("free -m").c_str());
-//        dstat_corountine();
+        dstat_corountine();
         if (!file_exists(name.c_str())) {
             int ret = mkdir(name.c_str(), 0755);
             if (ret != 0) {
@@ -631,7 +631,7 @@ namespace polar_race {
                 assert(thread_logical_cpu_id_.size() == 64);
                 // AIO
                 // Init aio context.
-                queue_depth = 4;
+                queue_depth = 32;
                 aio_ctx = 0;
                 iocb_ptrs = new iocb *[queue_depth];
                 iocbs = new iocb[queue_depth];
