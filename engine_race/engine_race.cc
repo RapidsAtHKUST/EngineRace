9ff653adf1263f2e71890e60f8d99dcd05f9a400
CHE Yulin <yche@cse.ust.hk>
2018-12-06 13:36:12 +0800

    test 64KB aio write

--- engine_race/engine_race.cc
+++ engine_race/engine_race.cc
@@ -207,9 +207,11 @@ namespace polar_race {
                     log_info("fd err of %d: %d, err info: %s", i, value_file_dp_[i], strerror(errno));
                     exit(-1);
                 }
+#ifdef ENABLE_FALLOCATE
                 size_t file_size = static_cast<uint64_t>(MAX_VAL_BUCKET_SIZE) * VALUE_SIZE * BUCKET_NUM / VAL_FILE_NUM;
                 log_info("File size: %zu", file_size);
                 fallocate(value_file_dp_[i], 0, 0, file_size);
+#endif
             }
             printTS(__FUNCTION__, __LINE__, clock_start);
 
